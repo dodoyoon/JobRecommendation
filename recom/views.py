cc075544eb6ffcc3f345bcb1f0b05c42a5475073
greentealemonade <kaydeee329@gmail.com>
2020-06-08 21:36:18 +0900

    display notice for personalized page - TOM for now

--- recom/views.py
+++ recom/views.py
@@ -43,13 +43,27 @@ def signup(request):
     return render(request, 'signup.html', {'form': form})
 
 def interest(request):
-    ctx = {
-    }
+    ctx = {}
 
     return render(request, 'interest.html', ctx)
 
 def personal(request):
-    ctx = {
-    }
+    ctx = {}
+
+    if request.user.is_authenticated:
+        username = request.user.username
+        user = request.user
+        ctx['userobj'] = user
+
+    username = 'Tom'
+    info = recom_models.User.objects.filter(name=username)
+    if info.exists():
+        ctx['name'] = info
+        loc = getattr(recom_models.User.objects.get(name=username), "location")
+        day = getattr(recom_models.User.objects.get(name=username), "holiday_tp_nm")
+
+
+    list = recom_models.Company.objects.raw('SELECT c.company_id, c.company, c.basic_addr, n.title, n.sal_tp_nm, n.max_sal, n.min_sal, n.holiday_tp_nm, n.min_edubg, n.career FROM company AS c JOIN notice AS n ON c.company_id = n.company_id JOIN user AS u ON c.region_id = (SELECT region_id FROM region WHERE %s = region) WHERE n.min_sal > 25000000 AND n.holiday_tp_nm = %s', [loc, day])
+    ctx['list'] = list
 
     return render(request, 'personalized.html', ctx)
